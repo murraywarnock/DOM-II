13c3a6a327edd346c4150a66ca67d14c4757a649
MurrayWarnock <murray.warnock@gmail.com>
2021-04-06 20:50:36 -0600

    Got most of the MVP done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // Your code goes here
+const navLink = document.querySelectorAll(".nav-link");
+const fbImg = document.querySelector(".intro img");
+
+fbImg.addEventListener("mouseover", function (event) {
+    fbImg.style.border = "5px solid red";
+});
+fbImg.addEventListener("mouseleave", function (event) {
+    fbImg.style.border = "";
+});
+
+document.addEventListener("keydown", function (event) {
+    console.log(event.key);
+    if (event.key === "f") {
+      alert("You hit the 'f' key!");
+    }
+  });
+
+window.addEventListener("load", function (event) {
+    alert("Fun Bus is Departing");
+});
+
+// navLinkArray = Array.from(navLink);
+// for (i = 0; i < navLinkArray; i++) {
+//     console.log(`navLinkArray[${i}]`);
+//     navLinkArray[i].addEventListener('mouseover', function(event) {
+//         navLinkArray[i].style.color = "red";
+//         console.log(`mouseover navLinkArray[${i}]`);
+//     })
+// }
+navLink[0].addEventListener('mouseover', function(event) {
+    navLink[0].style.color = "red";
+ });
+ navLink[0].addEventListener('mouseleave', function(event) {
+    navLink[0].style.color = "black";
+ });
+
+// cancelButton.addEventListener("click", function (event) {
+//     modal.classList.add("off");
+//     failureMessage.classList.remove("off");
+//   });
+  
